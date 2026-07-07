cec9adadd205e773b98fcbee0223e1df88b644c3
Selcuk Albut <salbut@gmail.com>
2026-07-07 13:29:21 +0300

    Milestone 1.1 - Project foundation and HomeView

---
+++ GSTCycling/Views/Home/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  GSTCycling
+//
+//  Created by Selcuk Albut on 7.07.2026.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
